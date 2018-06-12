0c4d73e0e027b1edfa3924a53e4cda5539369c1c
duanlians <737409663@qq.com>
2018-06-12 11:45:45 +0800

    relSearchAjax0.4

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,8 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
     'DEFAULT_MODULE'     => 'Index', //默认模块
-    'DEFAULT_CONTROLLER' =>  'Index', // 默认控制器名称
-    'DEFAULT_ACTION'     =>  'index', // 默认操作名称
+    'DEFAULT_CONTROLLER' => 'Index', // 默认控制器名称
+    'DEFAULT_ACTION'     => 'index', // 默认操作名称
     'URL_MODEL'          => '2', //URL模式
     'SESSION_AUTO_START' => true, //是否开启session
     'SESSION_PREFIX'     => 'xd_', // session 前缀
