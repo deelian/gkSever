8c233c77672538920cb50ea994ca6daf1a410daa
duanlians <737409663@qq.com>
2018-06-13 15:51:19 +0800

    debug

--- Application/Api/Controller/ListsController.class.php
+++ Application/Api/Controller/ListsController.class.php
@@ -17,38 +17,38 @@ class ListsController extends SearchController
     public function __construct()
     {
         parent::__construct();
-        $searchObj = new \XS(C('RES_SEARCH'));
+        $searchObj = new \XS('d_search');
         $this->XS = $searchObj->search;
         $this->XS->setCharset('UTF-8');
     }
 
     public function get($kw){
-//        $f = 'res_name';
-//        $q = get_magic_quotes_gpc() ? stripslashes($kw) : $kw;
-//        $s = 'id';
-//
-//        // fuzzy search
-//        $this->XS->setFuzzy(true);
-//
-//        // synonym search
-//        $this->XS->setAutoSynonyms(true);
-//
-//        // set query
-//        $this->XS->setQuery($f . ':(' . $q . ')');
-//
-//        // set sort
-//        if (($pos = strrpos($s, '_')) !== false) {
-//            $sf = substr($s, 0, $pos);
-//            $st = substr($s, $pos + 1);
-//            $this->XS->setSort($sf, $st === 'ASC');
-//        }
-//
-//        $search_begin = microtime(true);
-//        $lists = $this->XS->search();
-//        $search_cost = microtime(true) - $search_begin;
-//        return [
-//            'list'  => $lists,
-//            'costTime'  => $search_cost
-//        ];
+        $f = 'res_name';
+        $q = get_magic_quotes_gpc() ? stripslashes($kw) : $kw;
+        $s = 'id';
+
+        // fuzzy search
+        $this->XS->setFuzzy(true);
+
+        // synonym search
+        $this->XS->setAutoSynonyms(true);
+
+        // set query
+        $this->XS->setQuery($f . ':(' . $q . ')');
+
+        // set sort
+        if (($pos = strrpos($s, '_')) !== false) {
+            $sf = substr($s, 0, $pos);
+            $st = substr($s, $pos + 1);
+            $this->XS->setSort($sf, $st === 'ASC');
+        }
+
+        $search_begin = microtime(true);
+        $lists = $this->XS->search();
+        $search_cost = microtime(true) - $search_begin;
+        return [
+            'list'  => $lists,
+            'costTime'  => $search_cost
+        ];
     }
 }
