f0c6f2fea4f127936225e32a9782a951b11869ef
duanlians <737409663@qq.com>
2018-06-13 15:46:10 +0800

    debug

--- Application/Api/Controller/ListsController.class.php
+++ Application/Api/Controller/ListsController.class.php
@@ -23,32 +23,32 @@ class ListsController extends SearchController
     }
 
     public function get($kw){
-        $f = 'res_name';
-        $q = get_magic_quotes_gpc() ? stripslashes($kw) : $kw;
-        $s = 'id';
-
-        // fuzzy search
-        $this->XS->setFuzzy(true);
-
-        // synonym search
-        $this->XS->setAutoSynonyms(true);
-
-        // set query
-        $this->XS->setQuery($f . ':(' . $q . ')');
-
-        // set sort
-        if (($pos = strrpos($s, '_')) !== false) {
-            $sf = substr($s, 0, $pos);
-            $st = substr($s, $pos + 1);
-            $this->XS->setSort($sf, $st === 'ASC');
-        }
-
-        $search_begin = microtime(true);
-        $lists = $this->XS->search();
-        $search_cost = microtime(true) - $search_begin;
-        return [
-            'list'  => $lists,
-            'costTime'  => $search_cost
-        ];
+//        $f = 'res_name';
+//        $q = get_magic_quotes_gpc() ? stripslashes($kw) : $kw;
+//        $s = 'id';
+//
+//        // fuzzy search
+//        $this->XS->setFuzzy(true);
+//
+//        // synonym search
+//        $this->XS->setAutoSynonyms(true);
+//
+//        // set query
+//        $this->XS->setQuery($f . ':(' . $q . ')');
+//
+//        // set sort
+//        if (($pos = strrpos($s, '_')) !== false) {
+//            $sf = substr($s, 0, $pos);
+//            $st = substr($s, $pos + 1);
+//            $this->XS->setSort($sf, $st === 'ASC');
+//        }
+//
+//        $search_begin = microtime(true);
+//        $lists = $this->XS->search();
+//        $search_cost = microtime(true) - $search_begin;
+//        return [
+//            'list'  => $lists,
+//            'costTime'  => $search_cost
+//        ];
     }
 }
