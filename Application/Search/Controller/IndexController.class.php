1e3f7ecb73117c5e68dff330cbf5ef7c6f6bbacb
duanlians <737409663@qq.com>
2018-06-28 10:55:36 +0800

    robot

--- Application/Search/Controller/IndexController.class.php
+++ Application/Search/Controller/IndexController.class.php
@@ -78,7 +78,7 @@ class IndexController extends BaseController {
         $data = new dataModel();
         $res = $data->getInfo(I('get.code'));
         if (!isset($res['info']['res_desc'])){
-            $res['info']['res_desc'] = $res['info']['res_name'];
+            $res['info']['res_desc']['0'] = $res['info']['res_name'];
         }
         p($res,1);
         $this->assign('info', $res);
