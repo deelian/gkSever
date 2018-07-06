678ab0ee0be9aac93194d74824b4e61ecb147f40
duanlians <737409663@qq.com>
2018-07-06 14:32:36 +0800

    loadExpire

--- Application/Common/Conf/sys.php
+++ Application/Common/Conf/sys.php
@@ -18,6 +18,9 @@ return array(
     'POWERED_BY'            => 'Ebola Union',
     'WEB_SERVICE'           => 'Ebola Union Core Server',
 
+    'LIST_EXPIRE'           => 1800,
+    'SIDERBAR_EXPIRE'       => 1800,
+
     'USER_LOCK_PRE'         => 'User:LockList:',
     'CHAT_LOCK_PRE'         => 'Chat:LockList:'
 );
