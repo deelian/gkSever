286f7c17047448466e23f058df74b3fe16d97b8b
duanlians <737409663@qq.com>
2018-08-01 10:52:37 +0800

    udpateCoreFuncLastend

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -81,7 +81,6 @@ class Page{
         $this->parameter[$this->p] = '[PAGE]';
 //        $this->url = U(ACTION_NAME, $this->parameter);
         $this->url = '/'.ACTION_NAME.'/'.$this->parameter['key'].'/p/'.urlencode('[PAGE]').'.jsp';
-        pLog($this->url,'debugUrl');
         /* 计算分页信息 */
         $this->totalPages = ceil($this->totalRows / $this->listRows); //总页数
         if(!empty($this->totalPages) && $this->nowPage > $this->totalPages) {
