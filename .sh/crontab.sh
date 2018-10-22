7ea61eb53a7ded61095b213ee6a7d663aa8b4e72
duanlians <737409663@qq.com>
2018-10-22 18:01:47 +0800

    cronInfo

---
+++ .sh/crontab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+step=1 #间隔的秒数，不能大于60
+ 
+for (( i = 0; i < 45; i=(i+step) )); do
+    #cd /data/wwwroot/www.ebolaunion.gq && php index.php /Chat/Automessage/sendJoke
+    curl -s -m 10 --connect-timeout 10 -I http://www.ebolaunion.gq/sendJokes
+    sleep $step
+done
+ 
+exit 0
