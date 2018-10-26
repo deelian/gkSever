ff213f44f1b4949ccff1d7d5bae14e82eae7552f
duanlians <737409663@qq.com>
2018-10-26 18:11:20 +0800

    loadChatSysCp

--- Application/Admin_eu/Conf/config.php
+++ Application/Admin_eu/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+
     'TMPL_L_DELIM' => '{{',
     'TMPL_R_DELIM' => '}}',
 
--- Application/Admin_eu/Controller/ChatController.class.php
+++ Application/Admin_eu/Controller/ChatController.class.php
@@ -24,10 +24,10 @@ class ChatController extends BaseadminController
     public function __construct()
     {
         parent::__construct();
-        $this->ChatListPre = C('CHAT_LISTS_PRE');
+        $this->ChatListPre   = C('CHAT_LISTS_PRE');
         $this->sysSetChatPre = C('SYS_SET_CHAT_PRE');
-        $this->xkModel = new xk();
-        $this->cModel = new ChatListModel();
+        $this->xkModel       = new xk();
+        $this->cModel        = new ChatListModel();
     }
 
     public function chatInfoList()
@@ -76,7 +76,27 @@ class ChatController extends BaseadminController
                 ]
             ]
         );
-        $res = $this->cModel->where($where)->select();
+//        p($where,1);
+
+        $limit = 15;
+        $res   = $this->cModel->where($where)->order('updatetime DESC')->page($page . ',' . $limit)->select();
+        $this->assign('res', $res);
+
+        $count = $this->cModel->where($where)->count();
+        $this->assign('num', $count);
+
+        $Page = new \Think\Page($count, $limit);
+        $Page->setConfig('header', '');
+        $Page->setConfig('prev', 'Pre');
+        $Page->setConfig('next', 'Next');
+        $Page->setConfig('last', 'End');
+        $Page->setConfig('first', 'First');
+        $Page->setConfig('theme', '%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        $show = bootstrap_page_style($Page->show());
+        $this->assign('page', $show);
+
+        $this->display();
+//        p($res,1);
 
     }
 
@@ -99,15 +119,72 @@ class ChatController extends BaseadminController
         return false;
     }
 
-
-    public function updateChatInfo()
-    {
-
-    }
-
     public function addChatInfo()
     {
+        if (IS_AJAX) {
+            $req = I('');
+            if (!empty($req)) {
+                if (!empty($req['id'])) {
+                    //update
+                    $req['updateTime'] = time();
+//                    p($req,1);
+                    $res = $this->cModel->save($req);
+                } else {
+                    //addNew
+                    $req['info'] = explode(',', str_replace("\n",",",$req['info']));
+                    $sub = [];
+                    foreach($req['info'] as $k =>$v) {
+                        if (!empty($v)) {
+                            $time = time();
+                            $sub[$k] = [
+                                'info'       => $v,
+                                'level'      => $req['level'],
+                                'addTime'    => $time,
+                                'updateTime' => $time
+                            ];
+                        }
+                    }
+//                    p($sub,1);
+                    $res = $this->cModel->addAll($sub);
+//                    p($res,1);
+                }
 
+                if ($res) {
+                    $code = 200;
+                } else {
+                    $code = 501;
+                }
+                $this->ajaxReturn([
+                    'code'  => $code,
+                ]);
+            }
+        }
+        $req = I('get.id');
+        $info = $this->cModel->where(['id'  => $req])->find();
+//        p($info,1);
+        if ($info) {
+            $this->assign('info', $info);
+        }
+        $this->display('add');
+    }
+
+    public function del()
+    {
+        if (IS_POST) {
+            $req = I('post.');
+//            p($req,1);
+            $res = $this->cModel->delete($req['id']);
+            if ($res) {
+                $this->ajaxReturn([
+                    'code'  => 200
+                ]);
+            }
+        } else {
+            $this->ajaxReturn([
+                'code'  => 501,
+                'msg'   => 'Sys Error!'
+            ]);
+        }
     }
 
 //    public function firLoadInfo()
