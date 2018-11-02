57438027eeb792c4a1ed1817062b60f0e4fca740
duanlians <737409663@qq.com>
2018-11-02 14:35:04 +0800

    updateTempAdminPage

--- Application/Admin_eu/Controller/SysController.class.php
+++ Application/Admin_eu/Controller/SysController.class.php
@@ -14,54 +14,26 @@ use Home\Controller\XkController as xk;
 class SysController extends BaseadminController
 {
 
-    private $setMessage;
-    private $smRed;
-    private $smRedPre;
+    private $xkMod;
 
     public function __construct()
     {
         parent::__construct();
-        $this->smRedPre = C('SET_MESSAGE');
-        $this->smRed = new xk();
-        $this->setMessage = new sm();
+        $this->siteRedPre = C('SET_MESSAGE');
+        $this->xkMod = new xk();
+//        $this->setMessage = new sm();
     }
 
-    public function sysChatInfoSet()
+    public function index()
     {
-//        $this->setMessage;
-//        if ($this->smRed->RED->hmset($this->smRedPre, $data)) {
-//
-//        }
-    }
 
-    public function sysChatInfLists()
-    {
-        $list = $this->smRed->RED->hgetall($this->smRedPre);
-        if (empty($list)) {
-            $list = $this->sysChatInfoSet();
-        }
-        $sum = array_sum($list);
-
-        $limit = 20;
-        $Page  = new \Think\Page($sum, $limit);
-        $Page->setConfig('header','');
-        $Page->setConfig('prev','Pre');
-        $Page->setConfig('next','Next');
-        $Page->setConfig('last','End');
-        $Page->setConfig('first','First');
-        $Page->setConfig('theme','%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
-        $show  = bootstrap_page_style($Page->show());
-
-        $data = [
-            'list'  => $list,
-            'page'  => $show,
-            'sum'   => $sum
-        ];
-        $this->assign('data', $data);
         $this->display();
     }
 
-    public function sysChatInfoUpdate()
+    /**
+     * 系统chat设置列表
+     */
+    public function sysChatInfLists()
     {
 
     }
